ce5064bd04821d6d195ec33e4348fae1269aac9c
FahrulZul <fahrulzul96@gmail.com>
2023-01-05 15:22:13 +0800

    FCC Passed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
 import "./App.scss";
-import { useState } from "react";
-import Control from "./components/control.component";
+import { useState, useEffect, useRef } from "react";
+import Control from "./components/Control.jsx";
 
 const CONTROL_TYPE = {
     SESSION: "session",
     BREAK: "break",
 };
 
+const minutesToMillis = (min) => min * 60 * 1000;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
+
 function App() {
     const [isStart, setIsStart] = useState(false);
     const [length, setLength] = useState({
-        break: 5,
-        session: 25,
+        break: 0.1,
+        session: 0.1,
     });
+    const [timeType, setTimeType] = useState("session");
+    const [timeLeft, setTimeLeft] = useState(minutesToMillis(length.session));
+    const intervalId = useRef(null);
+    const alarm = useRef(null);
 
     const handleStartStop = () => {
         setIsStart(!isStart);
     };
+
+    useEffect(() => {
+        setTimeLeft(minutesToMillis(length.session));
+    }, [length]);
+
+    useEffect(() => {
+        if (isStart) {
+            intervalId.current = setInterval(() => {
+                setTimeLeft((timeLeft) => timeLeft - 1000);
+            }, 1000);
+        } else {
+            clearInterval(intervalId.current);
+        }
+
+        return () => clearInterval(intervalId.current);
+    }, [isStart]);
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            alarm.current.play();
+            if (timeType === "session") {
+                setTimeType("break");
+                setTimeLeft(minutesToMillis(length.break));
+            } else {
+                setTimeType("session");
+                setTimeLeft(minutesToMillis(length.session));
+            }
+        }
+    }, [timeType, timeLeft, length]);
 
     const handleLength = (event) => {
         const sessionType = event.currentTarget.value;
@@ -36,6 +72,21 @@ function App() {
         }
     };
 
+    const handleReset = () => {
+        clearInterval(intervalId.current);
+        setIsStart(false);
+        setLength({ session: 25, break: 5 });
+        setTimeType("session");
+        if (!alarm.current.ended) {
+            alarm.current.pause();
+            alarm.current.load();
+        }
+    };
+
+    const minute =
+        length.session === 60 ? 60 : Math.floor(timeLeft / 1000 / 60) % 60;
+    const seconds = Math.floor(timeLeft / 1000) % 60;
+
     return (
         <div className="App">
             <div className="container">
@@ -43,15 +94,15 @@ function App() {
                     <div className="pomodoro">
                         <div className="timer-box">
                             <p className="timer" id="time-left">
-                                25:00
+                                {formatTime(minute)}:{formatTime(seconds)}
                             </p>
                             <p className="timer-label" id="timer-label">
-                                Session
+                                {timeType === "session" ? "Session" : "Break"}
                             </p>
                         </div>
                         <button
                             className="btn btn-startStop"
-                            id="time-stop"
+                            id="start_stop"
                             onClick={handleStartStop}
                         >
                             <i className={isStart ? "ph-pause" : "ph-play"}></i>
@@ -69,10 +120,19 @@ function App() {
                             />
                         </div>
                         <div className="option-box">
-                            <button className="btn" id="reset">
+                            <button
+                                className="btn"
+                                id="reset"
+                                onClick={handleReset}
+                            >
                                 <i className="ph-clock-clockwise-bold"></i>
                             </button>
                         </div>
+                        <audio
+                            id="beep"
+                            ref={alarm}
+                            src="https://www.dropbox.com/s/wmwkzenkp7jh3ip/alarm-pomodoro.mp3?dl=1"
+                        />
                     </div>
                 </div>
             </div>
